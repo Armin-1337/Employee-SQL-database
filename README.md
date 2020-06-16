4778c3cfcc8dd182802cb4f93b660ff0595c954b
Armin-1337 <55409636+Armin-1337@users.noreply.github.com>
2020-06-15 21:41:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Employee-SQL-database
+ Used SQL to perform data modeling, engineering and analysis of an employee database. Plots using matplotlib.
---
+++ schema.sql
@@ -0,0 +1,91 @@
+﻿DROP TABLE IF EXISTS departments CASCADE ;
+DROP TABLE IF EXISTS employees CASCADE ;
+DROP TABLE IF EXISTS titles CASCADE ;
+DROP TABLE IF EXISTS dept_manager CASCADE ;
+DROP TABLE IF EXISTS salaries CASCADE ;
+DROP TABLE IF EXISTS dept_emp CASCADE ;
+
+CREATE TABLE "departments" (
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "id_de" serial   NOT NULL,
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "id_de"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "id_dm" serial   NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    "emp_no" int   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "id_dm","emp_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "gender" varchar   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "id_sal" serial   NOT NULL,
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "id_sal"
+     )
+);
+
+CREATE TABLE "titles" (
+    "id_title" serial   NOT NULL,
+    "emp_no" int   NOT NULL,
+    "title" varchar   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "id_title"
+     )
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+
+
